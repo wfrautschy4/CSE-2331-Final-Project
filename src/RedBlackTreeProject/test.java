2e333005916501c108845c457b3f5135b47f7c1f
AasamaPrabhakar2005 <41838058+AasamaPrabhakar2005@users.noreply.github.com>
2025-04-27 22:58:34 -0400

    deletion

--- src/RedBlackTreeProject/test.java
+++ src/RedBlackTreeProject/test.java
@@ -6,22 +6,34 @@ class test {
 
     public static void main(String args[]){
         RedBlackTree<Integer> rbt = new RedBlackTree<Integer>();
-        rbt.insert(10);
         //Insert n elements into the tree and print each step
         int n = 10;
-        for(int i = 1; i <= n; i++){
+        rbt.insert(10);
+        rbt.insert(100);
+        rbt.insert(50);
+        rbt.insert(20);
+        rbt.insert(90);
+        rbt.insert(150);
+        rbt.insert(12);
+        rbt.insert(15);
+        /* for(int i = 1; i <= n; i++){
             int randomNum = (int) Math.floor(Math.random() * 99 + 1);
             rbt.insert(randomNum);
-
+            
             //Print it
             rbt.getTree().verifyParentPointers();
             TreePrinter.printTree(rbt.getTree());
             System.out.println("----------------------------------");
-            rbt.delete(10);
             TreePrinter.printTree(rbt.getTree());
-        }
+        } */
+        System.out.println("----------------------------------");
+        TreePrinter.printTree(rbt.getTree());
 
-        
+
+        rbt.delete(15);
+
+        System.out.println("----------------------------------");
+        TreePrinter.printTree(rbt.getTree());
 
     }
 }
