0c79dbf20642154a4add05cc79c60739e2627dad
wfrautschy4 <frautschy.2@buckeyemail.osu.edu>
2025-04-27 23:44:34 -0400

    updated test

--- src/RedBlackTreeProject/test.java
+++ src/RedBlackTreeProject/test.java
@@ -7,7 +7,7 @@ class test {
 
 
     public static void main(String args[]){
-        testDelete();
+        timeDelete();
         
         
     }
@@ -18,7 +18,7 @@ public static void testDelete(){
     //Insert all the elements
     int numElem = 20;
     for (int i = 0; i < numElem; i++) {
-        int randomNum = (int) Math.floor(Math.random() * numElem + 1);
+        int randomNum = (int) Math.floor(Math.random() * 99 + 1);
         rbt.insert(randomNum);
 
     }
@@ -29,10 +29,12 @@ public static void testDelete(){
     ArrayList<Integer> list = rbt.getList();
     
     //Remove an element
-    System.out.println("Removing: "+ list.get(15));
-    rbt.delete(list.get(15));
-    TreePrinter.printTree(rbt.getTree());
-
+    for(int i = 0; i < rbt.size() * 2; i++){
+        System.out.println("Removing: "+ list.get(i));
+        rbt.delete(list.get(i));
+        TreePrinter.printTree(rbt.getTree());
+        System.out.println("------------------------");
+    }
 }
 
 
@@ -78,6 +80,29 @@ public static void timeSearch(){
 }
         
         
+public static void timeDelete(){
+    RedBlackTree<Integer> rbt = new RedBlackTree<Integer>();
+    int datapoints = 7;
+    for(int j = 1; j < datapoints * Math.pow(10,datapoints); j=j*10){
+        //Insert all the elements
+        for (int i = 0; i < j; i++) {
+            int randomNum = (int) Math.floor(Math.random() * j + 1);
+            rbt.insert(randomNum);
+        }
+
+        //Get list of all elements
+        ArrayList<Integer> list = rbt.getList();
+
+        //Delete each element
+        long start = System.nanoTime();
+        for (int i = 0; i < j; i++){
+            rbt.delete(list.get(i));
+        }
+        long end = System.nanoTime();
+            System.out.println("Average time for ("+j+") deletions: " + (end - start) / 1000 + " ns");
+        rbt.clear();
+    }
+}
         
 
 
